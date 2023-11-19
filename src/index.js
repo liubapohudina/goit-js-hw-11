d3b6b6df744c18b2505388fc33eb21bd4c816e9c
Liubov <drogaltseva92@gmail.com>
2023-11-19 16:58:24 +0100

    add field for search

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import Notiflix from 'notiflix';
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "40771201-2278ca32ba7eea467c30dfc24"
+
+const refs = {
+    searchForm: document.querySelector('.search-form')
+}
