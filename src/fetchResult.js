68ec5da13098e112b12ffa75f1da241802c3cad2
Liubov <drogaltseva92@gmail.com>
2023-11-20 10:01:01 +0100

    add task 1

--- src/fetchResult.js
+++ src/fetchResult.js
@@ -1,1 +1,15 @@
 import axios from "axios";
+
+export async function fetchResult(value, page) {
+    const URL = 'https://pixabay.com/api/'
+    const API_KEY = "40771201-2278ca32ba7eea467c30dfc24"
+    const params = new URLSearchParams ({
+        key: API_KEY,
+        q: value,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true
+    })
+         return  await axios.get(`${URL}?${params}`).then(response => response.data )
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-
 import Notiflix from 'notiflix';
 import axios from "axios";
+import { fetchResult } from './fetchResult';
 
 axios.defaults.headers.common["x-api-key"] = "40771201-2278ca32ba7eea467c30dfc24"
 
 const refs = {
-    searchForm: document.querySelector('.search-form')
+    searchForm: document.querySelector('#search-form'),
+    gallery: document.querySelector('.gallery')
+}
+console.log(refs.searchForm)
+let a = 5
+console.log(a)
+
+
+let page = 1;
+let searchWord = ''
+refs.searchForm.addEventListener('submit', handleSubmit);
+console.log(refs.gallery)
+async function handleSubmit(event) {
+    event.preventDefault();
+    searchWord = event.currentTarget.searchWord.value;
+    try {
+        const searchObjects = await fetchResult(searchWord, page);
+        const showObjects = searchObjects.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => ({
+            webformatURL, largeImageURL, alt: tags, likes, views, comments, downloads
+        }));
+        if (showObjects.length === 0) {
+            return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        }
+    
+    } catch (error) {
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    }
 }
